50d3a1f25dea27356e2f6c8556ab51300e133796
Henrique Campos <henriiquecampos@gmail.com>
2023-05-22 11:13:59 -0300

    Add can capture and piece enabling logic

--- source/06.building-online-checkers/CheckerBoard.gd
+++ source/06.building-online-checkers/CheckerBoard.gd
@@ -36,20 +36,15 @@ func map_pieces(team):
 
 
 func toggle_turn():
+	clear_free_cells()
 	if current_turn == Teams.BLACK:
 		current_turn = Teams.WHITE
-		for piece in black_team.get_children():
-			piece.disable()
-		for piece in white_team.get_children():
-			piece.enable()
+		disable_pieces(black_team)
+		enable_pieces(white_team)
 	else:
 		current_turn = Teams.BLACK
-		for piece in white_team.get_children():
-			piece.disable()
-		for piece in black_team.get_children():
-			piece.enable()
-	for child in free_cells.get_children():
-		child.queue_free()
+		disable_pieces(white_team)
+		enable_pieces(black_team)
 
 
 func _on_piece_selected(piece):
@@ -57,28 +52,96 @@ func _on_piece_selected(piece):
 
 
 func _on_free_cell_selected(free_cell_position):
+	move_selected_piece(local_to_map(free_cell_position))
+
+
+func move_selected_piece(target_cell):
 	var current_cell = local_to_map(selected_piece.position)
-	var free_cell = local_to_map(free_cell_position)
-	selected_piece.position = free_cell_position
+	selected_piece.position = map_to_local(target_cell)
 	
 	# Updates meta_board
 	meta_board[current_cell] = null
-	meta_board[free_cell] = selected_piece
-	
-	capture_pieces(current_cell, free_cell)
-	selected_piece.deselect()
-	
+	meta_board[target_cell] = selected_piece
+	crown()
 	toggle_turn()
+	selected_piece.deselect()
+
+
+func crown():
+	var selected_piece_cell = local_to_map(selected_piece.position)
+	
+	if selected_piece.team == Teams.BLACK and selected_piece_cell.y < 1:
+		selected_piece.is_king = true
+	elif selected_piece.team == Teams.WHITE and selected_piece_cell.y > 6:
+		selected_piece.is_king = true
+
+
+func enable_pieces(team):
+	var capturing_pieces = []
+	var available_pieces = []
+	
+	for piece in team.get_children():
+		var capturing = can_capture(piece)
+		if capturing:
+			capturing_pieces.append(piece)
+		else:
+			available_pieces.append(piece)
+	if capturing_pieces.size() > 0:
+		for piece in capturing_pieces:
+			piece.enable()
+			piece.set_capturing(true)
+	else:
+		for piece in available_pieces:
+			piece.enable()
+
+
+func disable_pieces(team):
+	for piece in team.get_children():
+		piece.disable()
+		piece.set_capturing(false)
+
+
+func can_capture(piece):
+	var directions = NEIGHBOR_CELLS_BLACK
+	if current_turn == Teams.WHITE:
+		directions = NEIGHBOR_CELLS_WHITE
+	if piece.is_king:
+		directions = NEIGHBOR_CELLS_KING
+	var capturing = false
+	for direction in directions:
+		var current_cell = local_to_map(piece.position)
+		var neighbor_cell = current_cell + direction
+		# Cell is out of the board's boundaries
+		if not neighbor_cell in meta_board:
+			continue
+		var cell_content = meta_board[neighbor_cell]
+		# Cell is occupied
+		if not cell_content == null:
+			# The content of the cell is an opponent piece
+			if not cell_content.team == piece.team:
+				var capturing_cell = neighbor_cell + direction
+				# There's no cells to move to after capturing, so capturing isn't possible
+				if not capturing_cell in meta_board:
+					continue
+				# There's a neighbor free cell in the capturing direction
+				cell_content = meta_board[capturing_cell]
+				if cell_content == null:
+					capturing = true
+	return capturing
 
 
 func capture_pieces(origin_cell, target_cell):
 	var direction = Vector2(target_cell - origin_cell).normalized()
-	direction = direction.round()
-	var cell = target_cell - Vector2i(direction)
+	direction = Vector2i(direction.round())
+	var cell = target_cell - direction
+	
+	if not cell in meta_board:
+		return
 	
 	var cell_content = meta_board[cell]
 	if cell_content:
 		cell_content.queue_free()
+		meta_board[cell] = null
 
 
 func select_piece(piece):
@@ -95,7 +158,9 @@ func select_piece(piece):
 		movement_directions = NEIGHBOR_CELLS_KING
 	
 	var selected_piece_cell = local_to_map(selected_piece.position)
-	search_available_cells(selected_piece_cell, movement_directions)
+	var available_cells = search_available_cells(selected_piece_cell, movement_directions)
+	for cell in available_cells:
+		add_free_cell(cell)
 
 
 func search_available_cells(current_cell, directions):
@@ -112,19 +177,29 @@ func search_available_cells(current_cell, directions):
 		# Cell is occupied
 		if not cell_content == null:
 			# The content of the cell is an opponent piece
-			if not cell_content.get_parent() == selected_piece.get_parent():
+			if not cell_content.team == selected_piece.team:
 				var capturing_cell = cell + direction
+				# There's no cells to move to after capturing, so capturing isn't possible
 				if not capturing_cell in meta_board:
 					continue
 				# There's a neighbor free cell in the capturing direction
-				if meta_board[capturing_cell] == null:
+				cell_content = meta_board[capturing_cell]
+				if cell_content == null:
 					capturing = true
+					# Checks if previous cells lead to capturing, otherwise they are removed
+					for available_cell in available_cells:
+						for _direction in directions:
+							var neighbor_cell = available_cell - _direction
+							if not neighbor_cell in meta_board:
+								continue
+							cell_content = meta_board[neighbor_cell]
+							# Removes cells that don't lead to capturing if
+							if cell_content == null or cell_content == selected_piece:
+								available_cells.erase(available_cell)
 					available_cells.append(capturing_cell)
 		elif not capturing:
 			available_cells.append(cell)
-
-	for cell in available_cells:
-		add_free_cell(cell)
+	return available_cells
 
 
 func clear_free_cells():
--- source/06.building-online-checkers/Piece.gd
+++ source/06.building-online-checkers/Piece.gd
@@ -3,11 +3,15 @@ extends Node2D
 signal selected
 signal deselected
 
+enum Teams{BLACK, WHITE}
+
+@export var team: Teams = Teams.BLACK
 @export var is_king = false: set = _set_is_king
 @export var king_texture = preload("res://06.building-online-checkers/WhiteKing.svg")
 
 @onready var area = $Area2D
-@onready var color_rect = $ColorRect
+@onready var selected_color_rect = $SelectColorRect
+@onready var capturing_color_rect = $CapturingColorRect
 @onready var sprite = $Sprite2D
 
 var is_selected = false
@@ -30,14 +34,14 @@ func _on_area_2d_input_event(viewport, event, shape_idx):
 func select():
 	get_tree().call_group("selected", "deselect")
 	add_to_group("selected")
-	color_rect.show()
+	selected_color_rect.show()
 	is_selected = true
 	selected.emit()
 
 
 func deselect():
 	remove_from_group("selected")
-	color_rect.hide()
+	selected_color_rect.hide()
 	is_selected = false
 	deselected.emit()
 
@@ -48,3 +52,7 @@ func enable():
 
 func disable():
 	area.input_pickable = false
+
+
+func set_capturing(capturing):
+	capturing_color_rect.visible = capturing
