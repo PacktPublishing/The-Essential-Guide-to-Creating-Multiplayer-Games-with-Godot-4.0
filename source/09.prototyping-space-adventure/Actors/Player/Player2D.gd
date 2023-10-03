de50091454e57b5a7b22bc42b743cd69be3eea23
Henrique Campos <henriiquecampos@gmail.com>
2023-10-03 14:55:41 -0300

    Add extrapolation, prediction, and server-side motion

--- source/09.prototyping-space-adventure/Actors/Player/Player2D.gd
+++ source/09.prototyping-space-adventure/Actors/Player/Player2D.gd
@@ -16,17 +16,16 @@ extends Node2D
 
 @rpc("any_peer", "call_local")
 func setup_multiplayer(player_id):
-	var self_id = multiplayer.get_unique_id()
-	var is_player = self_id == player_id
-	set_process(is_player)
-	set_physics_process(is_player)
-	set_process_unhandled_input(is_player)
-	camera.enabled = is_player
-	if is_player:
-		camera.make_current()
+	var is_server = multiplayer.is_server()
+	set_process(is_server)
+	set_physics_process(is_server)
+	set_process_unhandled_input(not is_server)
+	camera.enabled = not is_server
+	if is_server:
 		$InterpolationTimer.start()
 		$SynchronizationTimer.start()
-	set_multiplayer_authority(player_id)
+	else:
+		camera.make_current()
 
 
 func _unhandled_input(event):
@@ -34,59 +33,98 @@ func _unhandled_input(event):
 		weapon.rpc("set_firing", true)
 	elif event.is_action_released("shoot"):
 		weapon.rpc("set_firing", false)
+	
+	if event.is_action_pressed(thrust_action):
+		spaceship.rpc_id(1, "set_thrusting", true)
+	elif event.is_action_released(thrust_action):
+		spaceship.rpc_id(1, "set_thrusting", false)
+	
+	# Turning logic. If a turning key is just pressed or still pressed, the spaceship should turn.
+	if event.is_action_pressed(turn_left_action):
+		spaceship.rpc_id(1, "set_direction", -1)
+		spaceship.rpc_id(1, "set_turning", true)
+	elif event.is_action_released(turn_left_action):
+		if Input.is_action_pressed(turn_right_action):
+			spaceship.rpc_id(1, "set_direction", 1)
+		else:
+			spaceship.rpc_id(1, "set_turning", false)
+			spaceship.rpc_id(1, "set_direction", 0)
+	if event.is_action_pressed(turn_right_action):
+		spaceship.rpc_id(1, "set_direction", 1)
+		spaceship.rpc_id(1, "set_turning", true)
+	elif event.is_action_released(turn_right_action):
+		if Input.is_action_pressed(turn_left_action):
+			spaceship.rpc_id(1, "set_direction", -1)
+		else:
+			spaceship.rpc_id(1, "set_turning", false)
+			spaceship.rpc_id(1, "set_direction", 0)
 
 
-func _physics_process(delta):
-	if Input.is_action_pressed(thrust_action):
-		spaceship.thrust()
-	if Input.is_action_pressed(turn_left_action):
-		spaceship.turn(-1)
-	elif Input.is_action_pressed(turn_right_action):
-		spaceship.turn(1)
-
-
-@rpc("any_peer", "call_remote")
-func interpolate_position(target_position):
+@rpc("authority", "call_remote")
+func interpolate_position(target_position, duration_in_seconds):
 	var tween = create_tween()
 	
 	var interpolation = lerp(previous_position, target_position, 1.0)
 	
 	tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-	var tweener = tween.tween_property(spaceship, "position", interpolation, $InterpolationTimer.wait_time)
+	var tweener = tween.tween_property(spaceship, "position", interpolation, duration_in_seconds)
 	tweener.from(previous_position)
 	previous_position = target_position
 
 
-@rpc("any_peer", "call_remote")
-func interpolate_rotation(target_rotation):
+@rpc("authority", "call_remote")
+func interpolate_rotation(target_rotation, duration_in_seconds):
 	var tween = create_tween()
 	
 	var interpolation = lerp_angle(previous_rotation, target_rotation, 1.0)
 	
 	tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-	var tweener = tween.tween_property(spaceship, "rotation", interpolation, $InterpolationTimer.wait_time)
+	var tweener = tween.tween_property(spaceship, "rotation", interpolation, duration_in_seconds)
 	tweener.from(previous_rotation)
 	previous_rotation = target_rotation
 
 
-@rpc("any_peer", "call_remote")
+@rpc("authority", "call_remote")
 func synchronize_position(new_position):
+	for tween in get_tree().get_processed_tweens():
+		tween.stop()
+	
+	# Prediction
+	var distance = previous_position.distance_to(new_position)
+	var direction = previous_position.direction_to(new_position)
+	var linear_velocity = (direction * distance) / $SynchronizationTimer.wait_time
+	extrapolate_position(linear_velocity)
+	
 	spaceship.position = new_position
 	previous_position = new_position
 
 
-@rpc("any_peer", "call_remote")
+func extrapolate_position(linear_velocity):
+	spaceship.linear_velocity = linear_velocity
+
+
+@rpc("authority", "call_remote")
 func synchronize_rotation(new_rotation):
+	for tween in get_tree().get_processed_tweens():
+		tween.stop()
+	
+	# Prediction
+	var angular_velocity = lerp_angle(previous_rotation, new_rotation, 1.0) / $SynchronizationTimer.wait_time
+	extrapolate_rotation(angular_velocity)
+	
 	spaceship.rotation = new_rotation
 	previous_rotation = new_rotation
 
 
+func extrapolate_rotation(angular_velocity):
+	spaceship.angular_velocity = angular_velocity
+
+
 func _on_interpolation_timer_timeout():
-	rpc("interpolate_rotation", spaceship.rotation)
-	rpc("interpolate_position", spaceship.position)
+	rpc("interpolate_rotation", spaceship.rotation, $InterpolationTimer.wait_time)
+	rpc("interpolate_position", spaceship.position, $InterpolationTimer.wait_time)
 
 
 func _on_synchronization_timer_timeout():
-#	rpc("synchronize_position", spaceship.position)
-#	rpc("synchronize_rotation", spaceship.rotation)
-	pass
+	rpc("synchronize_position", spaceship.position)
+	rpc("synchronize_rotation", spaceship.rotation)
--- source/09.prototyping-space-adventure/Objects/Spaceship/Spaceship.gd
+++ source/09.prototyping-space-adventure/Objects/Spaceship/Spaceship.gd
@@ -5,12 +5,38 @@ extends RigidBody2D
 @export var acceleration = 600.0
 @export var turn_torque = 10.0
 
+@export var thrusting = false : set = set_thrusting
+@export var turning = false : set = set_turning
+@export_range(-1, 1, 1) var direction = 0 : set = set_direction
 
-func thrust():
-	var delta = get_physics_process_delta_time()
+
+func _physics_process(delta):
+	if thrusting:
+		thrust(delta)
+	if turning:
+		turn(delta)
+
+
+func thrust(delta):
 	linear_velocity += (acceleration * delta) * Vector2.RIGHT.rotated(rotation)
+	print("Thrusting caller is: %s" % multiplayer.get_unique_id())
 
 
-func turn(direction):
-	var delta = get_physics_process_delta_time()
+func turn(delta):
 	angular_velocity += (direction * turn_torque) * delta
+
+
+@rpc("any_peer", "call_local")
+func set_thrusting(is_thrusting):
+	print("Set thrusting caller is: %s" % multiplayer.get_remote_sender_id())
+	thrusting = is_thrusting
+
+
+@rpc("any_peer", "call_local")
+func set_turning(is_turning):
+	turning = is_turning
+
+
+@rpc("any_peer", "call_local")
+func set_direction(new_direction):
+	direction = new_direction
